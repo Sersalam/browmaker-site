8b6e3cfdcb6acd78f85360c671e1449cbd22b66b
Sersalam <adrenalin854@gmail.com>
2022-01-10 00:30:28 +0800

    Added Postgres

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Homepage(models.Model):
+	homepage_image = models.ImageField(upload_to='homepage_images/')
+	homepage_text = models.CharField(max_length=300)
